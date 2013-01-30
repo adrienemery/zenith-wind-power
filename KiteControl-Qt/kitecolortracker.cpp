1f90fa91dc5c0a55a4b7a99ecaafb5fd6276d21f
kyle.hounslow@gmail.com <kyle.hounslow@gmail.com@18a7bd23-4dfa-c7eb-c45e-90f9de95398a>
2013-01-30 01:48:25 +0000

    Update to kitecolortracker.cpp and kitecolortracker.h. SVN is up and running again! needed to checkout with "https" not "http" (pretty simple mistake with very dire consequences).

--- KiteControl-Qt/kitecolortracker.cpp
+++ KiteControl-Qt/kitecolortracker.cpp
@@ -9,12 +9,16 @@ KiteColorTracker::KiteColorTracker(QObject *parent) :
     src = 0;
     winName = "camStream";
 
+    //init HSV vals
+    _Hmin=0;_Smin=0;_Vmin=0;
+    _Hmax=255,_Smax=255,_Vmax=255;
+    _minArea=0;_maxArea=100;
+    _morphSize1 = 5;_morphSize2=5;_morphSize3=5;
+
     // create timer object
     timer = new QTimer(this);
     // connect timer to update slot
     connect(timer,SIGNAL(timeout()),this,SLOT(update()));
-
-    //this is a test COMMENT
 
     // start timer
     timer->start(sampleRate);
@@ -47,12 +51,79 @@ void KiteColorTracker::update()
         capture->read(currentFrame);
         cv::imshow(winName,currentFrame);
 
-    }else qDebug()<<"error acquiring webcam stream";
+        if(!currentFrame.empty())
+        filterKite(currentFrame);
 
-    emit updated();
+
+    }else qDebug()<<"error acquiring webcam stream";
 }
 void KiteColorTracker::cleanup(){
 
     delete capture;
+
+}
+
+//Change HSV values for filtering. Pass in values in H-S-V order
+void KiteColorTracker::setHSV(int Hmin=0, int Smin=0, int Vmin=0, int Hmax=255, int Smax=255 , int Vmax=255){
+    _Hmin = Hmin;
+    _Hmax = Hmax;
+    _Smin = Smin;
+    _Smax = Smax;
+    _Vmin = Vmin;
+    _Vmax = Vmax;
+
+}
+
+void KiteColorTracker::filterKite(cv::Mat frame){
+
+        std::vector< std::vector<cv::Point> > contours;
+        std::vector<cv::Vec4i> hierarchy;
+
+        //threshold image to filter wanted colour
+        cv::Mat temp,temp1;
+        //filter image and save it to temp1
+        cv::inRange(frame,cv::Scalar(_Hmin,_Smin,_Vmin),cv::Scalar(_Hmax,_Smax,_Vmax),temp);
+
+
+        //closing of contours. we dilate and erode with little rectangles
+        cv::Mat element = cv::getStructuringElement( cv::MORPH_RECT, cv::Size(_morphSize1,_morphSize1) );
+        cv::Mat element2 = cv::getStructuringElement( cv::MORPH_RECT, cv::Size(_morphSize2,_morphSize2) );
+        //dilating and erode filters out noise
+        cv::erode (temp,  temp1, element);
+        cv::dilate(temp1, temp1, element2 );
+        cv::dilate(temp1, temp1, element2 );
+
+        std::string win = "test";
+        cv::imshow(win,temp1);
+        //find contours of filtered image
+        cv::findContours(temp1,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );
+
+        //use moments method to find kite
+        double px=10,py=10,pr=10;
+        double refArea=0;
+        if (hierarchy.size() > 0) {
+            int index = 0;
+            for ( ; index >= 0; index = hierarchy[index][0]) {
+
+                cv::Moments moment = cv::moments((cv::Mat)contours[index]);
+                double area = moment.m00;
+
+                if(area >_minArea&&area<_maxArea){
+                    if(area>refArea){
+                        refArea=area;
+                        double x = moment.m10/area+1;
+                        px=x;
+                        double y = moment.m01/area+1;
+                        py=y;
+                        double r = sqrt(area/3.14);
+                        pr=r;
+
+                                      }
+                                     }
+
+                                   }
+                                 }
+
+
 
 }
